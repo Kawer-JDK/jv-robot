8dc10798cd8d689cdc480b5db3ef8184d1f1f83c
Jakub Kędzia <jkedzia99@gmail.com>
2026-09-08 21:22:17 +0200

    Added if when robot is on right place

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+    if (toX == robot.getX() && toY == robot.getY()) {
+        System.out.println("Robot is on right place");
+    }
     }
 }
